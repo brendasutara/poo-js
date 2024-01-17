d8b7b094980972bc470caecd2cbc25595c489f16
brendasutara <sutarabrenda@gmail.com>
2024-01-17 18:07:32 -0300

    Herencia de prototipos con la sintaxis de clases: extends

--- modulos/main.js
+++ modulos/main.js
@@ -40,7 +40,7 @@ function videoStop(id) {
 }
 
 //queremos ocultar la url de nuestros videos
-export class PlatziClass {
+class PlatziClass {
     constructor({
       name,
       videoID,
@@ -61,10 +61,14 @@ class Course {
     constructor({
         name,
         classes = [],
+        isFree = false,
+        lang = "spanish",
     }) {
         //poniendole el _ bloqueamos el cambio de cualquier persona no habilitada
         this._name = name;
         this.classes = classes;
+        this.isFree = isFree;
+        this.lang = lang;
     }
 
     get name() {
@@ -83,6 +87,7 @@ class Course {
 const cursoProgBasica = new Course({
     name: "Curso Gratis de Programación Básica",
     classes: futbolPOO,
+    isFree: true,
 })
 
 //Vamos a hacer como que name sea un metodo
@@ -94,6 +99,7 @@ const cursoDefinitivoHTML = new Course({
 });
 const cursoPracticoHTML = new Course({
     name: "Curso Practico de HTML y CSS",
+    lang: "english",
 });
 
 class LearningPath {
@@ -142,7 +148,11 @@ const escuelaVgs = new LearningPath({
     ],
 })
 
+
+
+
 //ESTE ES NUESTRO HERMOSO MOLDE-PROTOTIPO
+//CLASE MAMÁ
 class Student {
   constructor({
     name,
@@ -167,7 +177,50 @@ class Student {
   }
 }
 
-const juan2 = new Student({
+//CLASES HIJOS
+class FreeStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.isFree) {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn("Lo sentimos, " + this.name + ", solo puedes tomar cursos abiertos");
+    }
+  }
+}
+
+class BasicStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.lang !== "english") {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn("Lo sentimos, " + this.name + ", no puedes tomar cursos en inglés");
+    }
+  }
+}
+
+class ExpertStudent extends Student {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    this.approvedCourses.push(newCourse);
+  }
+}
+
+
+
+
+
+const juan2 = new FreeStudent({
   name: "JuanDC",
   username: "juandc",
   email: "juanito@juanito.com",
@@ -178,7 +231,7 @@ const juan2 = new Student({
   ],
 });
 
-const miguelito2 = new Student({
+const miguelito2 = new BasicStudent({
   name: "Miguelito",
   username: "migelitofeliz",
   email: "miguelito@juanito.com",
